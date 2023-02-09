957c654edb48178554de1bc4e171d579532f6224
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-02-09 18:17:57 -0500

    begin implementing obsidian-tasks types

--- src/scheduling.nim
+++ src/scheduling.nim
@@ -2,11 +2,11 @@ import std/osproc
 import std/tables
 import types
 
-proc notify(message: string, ntfyUrl: string) =
-  discard execProcess("curl", args=["-d", message, ntfyUrl], workingDir="", env=nil, options={poUsePath})
+proc notify(description: string, ntfyUrl: string) =
+  discard execProcess("curl", args=["-d", description, ntfyUrl], workingDir="", env=nil, options={poUsePath})
 
 proc sendNotificationsIfNeeded*(todos: TodoTable, ntfyUrl: string) =
   for filename, todos in pairs(todos.todosByFilename):
     for todo in todos:
-      notify(todo.message, ntfyUrl)
+      notify(todo.description, ntfyUrl)
 
--- src/types.nim
+++ src/types.nim
@@ -1,14 +1,46 @@
 import std/times
 import std/tables
+import regex
+
+# taken from https://github.com/obsidian-tasks-group/obsidian-tasks/blob/main/src/Task.ts
+let recurrenceSymbol* = '🔁'
+let startDateSymbol* = '🛫'
+let scheduledDateSymbol* = '⏳'
+let dueDateSymbol* = '📅'
+let doneDateSymbol* = '✅'
+let priorityRegex* = re'([⏫🔼🔽])$/u'
+let startDateRegex* = re'🛫 *(\d{4}-\d{2}-\d{2})$/u'
+let scheduledDateRegex* = re'[⏳⌛] *(\d{4}-\d{2}-\d{2})$/u'
+let dueDateRegex* = re'[📅📆🗓] *(\d{4}-\d{2}-\d{2})$/u'
+let doneDateRegex* = re'✅ *(\d{4}-\d{2}-\d{2})$/u'
+let recurrenceRegex* = re'🔁 ?([a-zA-Z0-9, !]+)$/iu'
+let hashTags* = re'(^|\s)#[^ !@#$%^&*(),.?":{}|<>]*'
 
 type
   Priority* {.pure.} = enum
-    High, Medium, Low
+    High, Medium, None, Low
+
+  Recurrence* {.pure.} = enum
+    # rrule: RRule
+    baseOnToday: bool
+    referenceDate: times.Time
+    startDate: times.Time
+    scheduledDate: times.Time
+    dueDate: times.Time
 
   Todo* = object
-    due*, start*: times.Time
-    message*: string
     priority*: Priority
+    status*: Status
+    description*: string
+    priority*: Priority
+    startDate*: times.Time
+    scheduledDate*: times.Time
+    dueDate*: times.Time
+    doneDate*: times.Time
+    recurrence*: Recurrence
+    tags*: seq[string]
+    # originalMarkdown: string
+    # scheduledDateIsInferred: bool
 
   TodoTable* = object
     # Todos sorted by how soon their deadlines are
@@ -18,3 +50,9 @@ type
 
 proc nextTodo(todoTable: TodoTable): Todo =
   return todoTable.schedule[0]
+
+let prioritySymbols* = initTable([Priority, string])
+prioritySymbols[Priority.High] = '⏫'
+prioritySymbols[Priority.Medium] = '🔼'
+prioritySymbols[Priority.Low] = '🔽'
+prioritySymbols[Priority.None] = ''
