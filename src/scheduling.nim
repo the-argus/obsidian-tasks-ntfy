bce945c8425fc9c301d0936bd4892c9f74891671
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-02-09 19:50:16 -0500

    remove code that could spam notifs

--- src/scheduling.nim
+++ src/scheduling.nim
@@ -6,7 +6,5 @@ proc notify(description: string, ntfyUrl: string) =
   discard execProcess("curl", args=["-d", description, ntfyUrl], workingDir="", env=nil, options={poUsePath})
 
 proc sendNotificationsIfNeeded*(todos: TodoTable, ntfyUrl: string) =
-  for filename, todos in pairs(todos.todosByFilename):
-    for todo in todos:
-      notify(todo.description, ntfyUrl)
+  discard
 
